7266d3734b70e951abce555c406283068ef773d6
cunli <cunli@cisco.com>
2014-06-11 11:04:35 +0800

    My answer to Q1.

--- src/com/duolcr/ctci/chapter1/Q1.java
+++ src/com/duolcr/ctci/chapter1/Q1.java
@@ -1,5 +1,30 @@
 package com.duolcr.ctci.chapter1;
 
 public class Q1 {
-
+	
+	public static boolean isUnique(String str, boolean caseSensitive) throws Exception{
+		if(str == null || str.length() == 0){
+			throw new NullPointerException();
+		}
+		else{
+			if(!caseSensitive) str = str.toLowerCase();
+			for(int i = 0; i < str.length(); i++){
+				for(int j = i+1; j < str.length(); j++){
+					if(str.charAt(i) == str.charAt(j)) return false;
+				}
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args){
+		String str = "E=-s0";
+		try {
+			System.out.println(isUnique(str, false));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
 }
